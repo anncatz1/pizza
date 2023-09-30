b255305626c6c8dc7ca47784b3389d45fdce0980
Annie Xu <anniexu@Annies-MacBook-Pro-4.local>
2023-09-30 15:48:39 -0500

    init vite project

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseQuantity, increaseQuantity } from "./cartSlice";
+
+//eslint-disable-next-line
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={() => dispatch(decreaseQuantity(pizzaId))}>
+        -
+      </Button>
+
+      <span className="text-sm font-medium">{currentQuantity}</span>
+
+      <Button type="round" onClick={() => dispatch(increaseQuantity(pizzaId))}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
